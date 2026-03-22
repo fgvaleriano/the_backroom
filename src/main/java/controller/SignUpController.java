a2426201d6f48917e7b3431741d1c5fc25ff815d
Lander <gabaslander@gmail.com>
2026-03-22 17:52:12 +0800

    Teh asya adi an sign up and stuff, and also an fxml file for signUP ada sa resources chekc nlang

---
+++ src/main/java/controller/SignUpController.java
@@ -0,0 +1,91 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+//This class handles with controlling the components in the fxml file....
+public class SignUpController implements Initializable {
+    //this would get the components in the fxml, and it must be the names are all the same....
+    @FXML private Label errorLabel;
+    @FXML private TextField usernameField;
+    @FXML private PasswordField passField;
+    @FXML private PasswordField confirmPassField;
+    @FXML private Button signUpBtn;
+
+
+    @FXML
+    public void signUp(){
+        //set style color...for clicking the button, style choice.. (this means, every chang, we have to change everything back..)
+        signUpBtn.setStyle("-fx-background-color: rgba(74, 91, 109, 1); -fx-background-radius: 10;");
+        String user = usernameField.getText();
+        String pass = passField.getText();
+        String confirmPass = confirmPassField.getText();
+
+        if(user.isEmpty()){
+            errorLabel.setText("Please input your username.");
+            hideError(false);
+        }else if(pass.isEmpty() || confirmPass.isEmpty() ){
+            errorLabel.setText("Please input your password.");
+            hideError(false);
+        } else if(!pass.equals(confirmPass)){
+            errorLabel.setText("Passwords don't match.");
+            hideError(false);
+        } else{
+            System.out.println("Welcome new member!!!");
+        }
+
+        signUpBtn.setStyle("-fx-background-color: rgb(99, 121, 145); -fx-background-radius: 10;");
+    }
+
+    public void hideError(boolean hide){
+        if(hide){
+            errorLabel.setVisible(false);
+            //errorLabel.setManaged(false);
+        }else{
+            errorLabel.setVisible(true);
+            //errorLabel.setManaged(true);
+        }
+    }
+
+    //this means that when a window opens do this things first....
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        hideError(true);
+    }
+
+    @FXML
+    public void signUpEnter(){
+        signUpBtn.setStyle("-fx-background-color: rgba(74, 91, 109, 1); -fx-background-radius: 10;");
+    }
+
+    @FXML
+    public void signUpExit(){
+        signUpBtn.setStyle("-fx-background-color: rgb(99, 121, 145); -fx-background-radius: 10;");
+    }
+
+    //this methods is to hover away from the fields, artistic choice....just rewiring the focus elswehere here the errorLabel...
+    @FXML
+    public void greenClick(){
+        errorLabel.getScene().getRoot().requestFocus();
+    }
+
+    @FXML
+    public void leftPClick(){
+        errorLabel.getScene().getRoot().requestFocus();
+    }
+
+    @FXML
+    public void rightPClick(){
+        errorLabel.getScene().getRoot().requestFocus();
+    }
+
+
+}
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,42 @@
+package main;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        showSignUp(primaryStage);
+    }
+
+    public void showSignUp(Stage stage){
+        try{
+            //Note the fxml file is already the ui class file...so no need to create another ui class file
+
+            FXMLLoader signUp = new FXMLLoader(getClass().getResource("/ui/SignUp.fxml"));
+            //no need to set up the controller here since we already did that na sa scene builder...
+
+            Parent root = signUp.load();
+            Scene scene = new Scene(root);
+
+            stage.setScene(scene);
+            stage.show();
+        }catch (Exception e)  {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
+
