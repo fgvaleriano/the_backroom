1b33fc5f7ea74a66f7ae3ba51988616be626bb2f
Lander <gabaslander@gmail.com>
2026-03-26 23:15:03 +0800

    Currently did both backend and front end sa Add Archive kay i realize its a bit crazy, check it out nlang

--- src/main/java/controller/AddArchiveController.java
+++ src/main/java/controller/AddArchiveController.java
@@ -1,17 +1,16 @@
 package controller;
 
-import javafx.beans.binding.Bindings;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,43 +18,237 @@ public class AddArchiveController implements Initializable {
     @FXML private Label addArchiveLabel;
 
     @FXML private AnchorPane root;
-    @FXML private VBox box;
+    @FXML private VBox mainBox1;
     @FXML private GridPane nameGrid;
 
     @FXML private Label nameLabel;
 
+    @FXML private Button nxtBtn;
+    @FXML private Button addBtn;
+    @FXML private Button removeBtn;
+
+
+    @FXML private TextField nameField;
+    @FXML private ComboBox typeMenu;
+    @FXML private TextArea synopField;
+    @FXML private TextField dateField;
+
+    //---------Book Variables--------
+    @FXML private ComboBox authorMenu;
+    private ObservableList<String> authors;
+    private FilteredList<String> authorFilter;
+    private boolean pickedAnAuthor = false;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        typeMenu.getItems().addAll("Movie", "TV Shows", "Books", "Games");
+        typeMenu.getStylesheets().add(getClass().getResource("/ui/MediaType_ComboBox.css").toExternalForm());
 
-        //we add a listener to listen to dynamically scale the components...
-        root.sceneProperty().addListener(((obs, prevScene, currScene) ->{
-            if(currScene != null) scale(currScene);
-        }));
+        //there is still an issue  with the synopField, dont knwo what to do with it yet...
+        synopField.getStylesheets().add(getClass().getResource("/ui/AddArchive_TextArea.css").toExternalForm());
 
+        setAuthorMenu();
+        authorMenu.setItems(authorFilter);
+        authorMenu.getStylesheets().add(getClass().getResource("/ui/MediaType_ComboBox.css").toExternalForm());
+        addListenerAuthorMenu();
+    }
+
+    @FXML
+    public void nextAdd(){
+        String name = nameField.getText();
+        String type = (String) typeMenu.getSelectionModel().getSelectedItem();
+        String synop = synopField.getText();
+        String date = dateField.getText();
+
+        //System.out.println("Name: " + name);
+        checkEmpty(name, type, synop, date);
 
     }
 
-    private void scale(Scene scene){
-        //Title Label
-        addArchiveLabel.styleProperty().bind(scene.heightProperty().multiply(0.0625).
-                asString("-fx-font-size: %.0fpx; -fx-font-family: 'Reddit Sans ExtraBold';"));
 
-        //this listen for any height changes for the vbox where label is siiting, so that margin is dynamically increased as well
-        box.heightProperty().addListener(((obs, prevBox, currBox) -> {
-            VBox.setMargin(addArchiveLabel, new Insets(currBox.doubleValue() * 0.0520833, 0, 0, 0));
 
-        }));
+    public boolean checkEmpty(String name, String type, String synop, String date){
+        boolean hasEmpty = false;
 
-        nameLabel.styleProperty().bind(nameGrid.widthProperty().multiply(0.09).multiply(0.3)
-                .asString("-fx-font-size: %.0fpx; -fx-font-family: 'Roboto Serif';"));
+        if(name.trim().isEmpty()){//removes any spaces, newlines to check if there is actual content
+            nameField.setStyle("-fx-border-color: #bb443c; -fx-border-radius: 10; -fx-border-width: 2; -fx-background-radius: 10;");
+            hasEmpty = true;
+        }else{
+            nameField.setStyle("-fx-border-color: transparent; -fx-border-radius: 0; -fx-border-width: 2; -fx-background-radius: 10;");
+        }
 
-        scene.widthProperty().addListener((obs, oldWidth, newWidth) -> {
-            System.out.println("Scene Width: " + newWidth);
+        if(type == null){
+            typeMenu.setStyle("-fx-border-color: #bb443c; -fx-border-radius: 10; -fx-border-width: 2;");
+            hasEmpty = true;
+        }else{
+            typeMenu.setStyle("-fx-border-color: transparent; -fx-border-width: 0;");
+        }
+
+        if(synop.trim().isEmpty()){ //removes any spaces, newlines to check if there is actual content
+            //for the kuan we assume since this add archive is moderator lang, we assume valid input na..so no more crazy stuff..
+            synopField.setStyle("-fx-border-color: #bb443c; -fx-border-radius: 20; -fx-border-width: 2; -fx-background-radius: 20;");
+            hasEmpty = true;
+        }else{
+            synopField.setStyle("-fx-border-color: transparent; -fx-border-width: 0;");
+        }
+
+        if(date.trim().isEmpty()){//removes any spaces, newlines to check if there is actual content
+            dateField.setStyle("-fx-border-color: #bb443c; -fx-border-radius: 10; -fx-border-width: 2; -fx-background-radius: 10;");
+            hasEmpty = true;
+        }else{
+            dateField.setStyle("-fx-border-color: transparent; -fx-border-radius: 0; -fx-border-width: 2; -fx-background-radius: 10;");
+        }
+        return hasEmpty;
+    }
+
+
+
+
+
+    //---------------Hover Style for buttons-------------------
+    @FXML
+    public void nextEnter(){
+        nxtBtn.setStyle("-fx-background-color:  #855421; -fx-background-radius: 10;");
+    }
+
+    @FXML
+    public void nextExit(){
+        nxtBtn.setStyle("-fx-background-color:  rgba(185, 120, 51, 1); -fx-background-radius: 10;");
+    }
+
+    @FXML
+    public void addEnter(){
+        addBtn.setStyle("-fx-background-color:  #855421; -fx-background-radius: 10;");
+    }
+
+    @FXML
+    public void addExit(){
+        addBtn.setStyle("-fx-background-color:  rgba(185, 120, 51, 1); -fx-background-radius: 10;");
+    }
+
+    @FXML
+    public void rmvEnter(){
+        removeBtn.setStyle("-fx-background-color:  #855421; -fx-background-radius: 10;");
+    }
+
+    @FXML
+    public void rmvExit(){
+        removeBtn.setStyle("-fx-background-color:  rgba(185, 120, 51, 1); -fx-background-radius: 10;");
+    }
+
+    //------------------------------------------------------------
+
+
+
+    //This unfocsing anything on the text fields, and stuff...
+    @FXML
+    public void bg1Click(){
+        addArchiveLabel.getScene().getRoot().requestFocus();
+    }
+
+    @FXML
+    public void bg2Click(){
+        addArchiveLabel.getScene().getRoot().requestFocus();
+    }
+
+
+    //To be added methods pa toh:
+    @FXML
+    public void goBack(){}
+
+    @FXML
+    public void saveMedia(){}
+
+    @FXML
+    public void removeAuthor(){}
+
+
+    //------------Methods for Putting Values on Combo Boxes and Adding Listeners-------------------
+    public void setAuthorMenu(){
+        authors = FXCollections.observableArrayList("Saori", "Dalia", "Marika", "Rika");
+        authorFilter = new FilteredList<>(authors);
+    }
+
+    public void addListenerAuthorMenu(){
+        authorMenu.getEditor().textProperty().addListener((obs, oldVal, input) -> {
+
+            //this also let to not do anything below if the person is just picking.....
+            if(!authorMenu.isFocused()) return; //this just safeguard kay theres an error if person is typing and then picking
+
+            authorMenu.getSelectionModel().clearAndSelect(-1);
+
+            //this is our condition on what we want to show on the authorMenu
+            authorFilter.setPredicate(item -> {
+                //if no user input show original list
+                if(input.isEmpty() || input == null) return true;
+
+                //return if any similar input
+                return item.toLowerCase().startsWith(input.toLowerCase());
+            });
+
+            //we force the dropDown Menu to show if user is typing
+            if(authorMenu.isFocused()){
+                authorMenu.hide();
+                authorMenu.show();
+            }
+
+            authorMenu.getSelectionModel().clearSelection();
         });
 
-        scene.heightProperty().addListener((obs, oldHeight, newHeight) -> {
-            System.out.println("Scene Height: " + newHeight);
+        //this method is for if the user focused on the combobox and then refocus again and did something we clear the selection again
+        authorMenu.getEditor().focusedProperty().addListener((obs, oldVal, currentlyFocus) -> {
+            if(currentlyFocus){
+                authorMenu.hide();
+                authorMenu.show();
+                authorMenu.getSelectionModel().clearAndSelect(-1);
+            }else{
+
+                //we only clear if its not on the adding na btn...kay  technically they lost focus
+                if(root.getScene().getFocusOwner() != addBtn) {
+                    //this is when if the user typed something and focus elsewhere and not select it then we clear it automatically
+                    authorMenu.getEditor().clear();
+                    authorMenu.getSelectionModel().clearAndSelect(-1);
+                    authorMenu.hide();
+                    authorFilter.setPredicate(null); //this reset the authorFilter
+
+                    //System.out.println("Focused elswhere clearing everything....");
+                    /*if(authorMenu.getSelectionModel().getSelectedItem() == null){
+                        System.out.println("Current select after clearing: " + authorMenu.getSelectionModel().getSelectedItem());
+                    }*/
+                }
+            }
+
         });
     }
+
+    //since ediatble say, when a user type and then select the item they need, we set the text to that...
+    @FXML public void pickAuthor(){
+        authorMenu.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, picked) -> {
+            if(picked != null){
+                authorMenu.getEditor().setText(picked.toString());
+                //System.out.println("Currently Picked: " + picked.toString());
+            }
+        });
+
+    }
+    //---------------------------------------------------------------------------------------------
+
+    //---------------------Book Button Methods-----------------------------------------------------
+    @FXML
+    public void addAuthor(){
+        String author = authorMenu.getSelectionModel().getSelectedItem().toString();
+
+        if(author != null){
+            System.out.println("Author Picked: " + author);
+            authorMenu.getSelectionModel().clearAndSelect(-1);
+            authorFilter.setPredicate(null);
+
+            //if previously there is no author pa sa list, and they need it, so we add it to the list...
+            if(!authors.contains(author)){
+                authors.add(author);
+            }
+        }
+    }
+
+
 }
