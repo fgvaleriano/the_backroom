58f09dadf67ba9dbe4aba8a1f1ffde095838f38d
Avril Dominique <avrildominique24@gmail.com>
2026-04-20 19:09:56 +0800

    minor changes added, just committing to checkout different branch

--- src/main/java/edu/tangingina/thebackroom/controller/AddArchive_v2.java
+++ src/main/java/edu/tangingina/thebackroom/controller/AddArchive_v2.java
@@ -1,22 +1,26 @@
 package edu.tangingina.thebackroom.controller;
 
-import edu.tangingina.thebackroom.util.FontLoader;
+import edu.tangingina.thebackroom.util.*;
 import javafx.fxml.*;
 import javafx.geometry.Pos;
 import javafx.scene.*;
 import javafx.event.*;
 import javafx.scene.control.*;
+import javafx.scene.image.*;
 import javafx.scene.layout.*;
 import javafx.stage.*;
 
 
 public class  AddArchive_v2 {
     private static Scene scene;
+    private static Image btnImage;
+    private static ComboBox<String> mediaTypeSelector;
     private static StackPane bkgCard;
     private static Stage window;
-    private static VBox innerCard, formContent;
+    private static VBox innerCard, formContent, dynamicForm;
     private static ScrollPane inputHolder, sp;
     private static Label header;
+    private static Button addBtn;
 
 
     public static void addArchiveView() {
@@ -35,13 +39,26 @@ public class  AddArchive_v2 {
         inputHolder = getInputHolder();                     //scrolable pane for the formContent
         inputHolder.setContent(formContent);
 
-        //header component, "Add Archive" text
+        //---- HEADER COMPONENT ----
         header = new Label("Add Archive");
         header.setFont(FontLoader.extra(30));
         header.getStyleClass().add("header-text");
 
         header.setMaxWidth(Double.MAX_VALUE);
         header.setAlignment(Pos.CENTER);
+
+        //---- ADD BUTTON ----
+        addBtn = new Button();
+        addBtn.getStyleClass().add("image-button");
+        btnImage = new Image(AddArchive_v2.class.getResourceAsStream(
+                "edu/tangingina/thebackroom/assets/add_btn.png"));
+        ImageView imageView = new ImageView(btnImage);
+        imageView.setFitWidth(150);
+        imageView.setPreserveRatio(true);
+
+        //will be hidden until media is selected
+        addBtn.setVisible(false);
+        addBtn.setManaged(false);
 
         innerCard.getChildren().addAll(header, inputHolder);
 
@@ -85,4 +102,28 @@ public class  AddArchive_v2 {
         VBox.setVgrow(sp, Priority.ALWAYS);                 //scrollpane expand to take up the rest of the card
         return sp;
     }
+
+    //media type selector
+    private static ComboBox getMediaType() {
+        mediaTypeSelector = new ComboBox<>();
+        mediaTypeSelector.getItems().addAll("Books", "Movies", "Film and TV Show");
+        mediaTypeSelector.setPromptText("Select Media Type");
+
+        //dyanmic listener
+        mediaTypeSelector.setOnAction(e -> {
+            String selected = mediaTypeSelector.getValue();
+            System.out.println("Media type: " + selected);
+        });
+
+        return mediaTypeSelector;
+    }
+
+    //dynamic container that changes depending on the type of media selected
+    private static VBox getDynamicForm() {
+        dynamicForm = new VBox();
+        dynamicForm.getStyleClass().add("form-content");
+
+        return dynamicForm;
+    }
+
 }
